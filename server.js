0b8422c59505214e6d28d3d3d66f5b01348cd8e9
wade-web <freedevweb221@gmail.com>
2025-10-06 18:05:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 // Connexion MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.MONGODB_URI || 'https://backend-tp-km23.onrender.com', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -20,4 +24,5 @@ app.use('/api/agent', require('./routes/agentRoutes'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Serveur Agent démarré sur le port ${PORT}`);
+
 });
