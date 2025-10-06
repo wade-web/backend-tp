f20129df9d6c73e453e37e79fb3843c45d25170e
wade-web <freedevweb221@gmail.com>
2025-10-06 19:12:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: ['https://tp-react-snowy.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 Connexion MongoDB
@@ -26,6 +30,7 @@ app.listen(PORT, () => {
   console.log(`Serveur Agent démarré sur le port ${PORT}`);
 
 });
+
 
 
 
